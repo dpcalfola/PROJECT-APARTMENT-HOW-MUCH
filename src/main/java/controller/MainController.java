0643743df4cb0e19d63bfc60bd429214acf4422c
Fola Flor <dpcalfola@gmail.com>
2022-02-14 18:28:16 +0900

    branch

--- src/main/java/controller/MainController.java
+++ src/main/java/controller/MainController.java
@@ -2,7 +2,10 @@ package controller;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Pane;
 import javafx.scene.text.Text;
 
 import java.io.IOException;
@@ -16,6 +19,10 @@ public class MainController implements Initializable {
 
 
     @FXML
+    private BorderPane mainPane;
+
+
+    @FXML
     private void handleSearchButtonAction(ActionEvent event) throws IOException {
         System.out.println("Search Button clicked !!");
         statusDisplayText.setText("Search Button clicked");
@@ -25,6 +32,9 @@ public class MainController implements Initializable {
     private void handleLoginButtonAction(ActionEvent event) throws IOException {
         System.out.println("Login Button clicked !!");
         statusDisplayText.setText("Login Button clicked");
+        Pane view = FXMLLoader.load(getClass().getResource("login-view.fxml"));
+        mainPane.setCenter(view);
+
     }
 
     @FXML
