57fa5e2280e05d2017c130c72375181076ce7c46
Fola Flor <dpcalfola@gmail.com>
2023-02-09 19:32:11 +0900

    Deploy > for macOS deploy setup
        1. env setting
            env information is going to be expired in 2023-02-26
    
        2. Dependencies setting
            - JDK: OpenJDK 19.0.2
            - DB driver ( from Maven )
            - JavaFX 19

--- src/main/java/com/folaSmile/apartSearch/databaseModel/ConnectDB.java
+++ src/main/java/com/folaSmile/apartSearch/databaseModel/ConnectDB.java
@@ -11,7 +11,7 @@ import java.sql.DriverManager;
 public class ConnectDB {
 
     Env env = new Env();
-    EnvVO envVO = env.getEnvData(2);
+    EnvVO envVO = env.getEnvData(3);
 
     String url = envVO.getUrl();
     String user = envVO.getUser();
